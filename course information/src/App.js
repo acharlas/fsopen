c28758eaa406fee5f7d91e6d1234ba71ba10240d
acharlas <acharlas@student.42.fr>
2021-06-09 12:08:16 +0200

    2.5

--- course information/src/App.js
+++ course information/src/App.js
@@ -1,50 +1,5 @@
 import React from 'react'
-
-const Courses = ({courses}) => {
-  return (
-    <>
-      {courses.map(course => <Course key={course.id} course={course}/>)}
-    </>
-  )
-}
-
-const Course = ({course}) => {
-  return (
-    <div>
-      <Header name={course.name}/>
-      <Content parts={course.parts}/>
-      <Total parts={course.parts}/>
-    </div>
-  )
-}
-
-const Header = ({name}) => {
-  return(
-    <div>
-      <h1>{name}</h1>
-    </div>
-  )
-}
-
-const Content = ({parts}) => {
-  return (
-    <>
-      {parts.map(part => <Part key={part.id} part={part}/>)}
-    </>
-  )
-}
-
-const Part = ({part}) => <p>{part.name} {part.exercises}</p>
-
-const Total = ({parts}) => {
-  const total = parts.reduce((sum,part) => 
-  sum += part.exercises, 0)
-  return (
-    <div>
-      <p>Number of exercises {total}</p>
-    </div>
-  )
-}
+import Courses from './components/Courses'
 
 const App = () => {
   const courses = [
